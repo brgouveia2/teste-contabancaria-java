4f00524665e2147a3b3202a3b70bf6cf35af924e
brgouveia2 <bruno.gouveiafarias@gmail.com>
2024-07-31 18:37:30 -0300

    mod

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,6 +1,30 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        // TO-DO: 
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Solicitando os dados ao usuário
+        System.out.println("Por favor, digite o número da Agência:");
+        String agencia = scanner.nextLine();
+
+        System.out.println("Por favor, digite o número da Conta:");
+        int numero = scanner.nextInt();
+        scanner.nextLine();  // Consumir a quebra de linha deixada pelo nextInt()
+
+        System.out.println("Por favor, digite o nome do Cliente:");
+        String nomeCliente = scanner.nextLine();
+
+        System.out.println("Por favor, digite o saldo inicial:");
+        double saldo = scanner.nextDouble();
+
+        // Exibindo a mensagem final
+        String mensagem = "Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, "
+                        + "sua agência é " + agencia + ", conta " + numero + " e seu saldo "
+                        + saldo + " já está disponível para saque.";
+        System.out.println(mensagem);
+
+        scanner.close();
     }
 }
+
